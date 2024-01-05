236e27eb5967e77fe3415f833aad3276fbfd205b
shavor <shavor80@yandex.ru>
2024-01-05 14:26:55 +0300

    Create randomChar.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
-import MarvelService from './services/MarvelService';
 
 import './style/style.scss';
 
-const marvelService = new MarvelService();
-// marvelService.getAllCharacters().then(item => item.data.results.forEach(el => console.log(el.id)));
-marvelService.getCharacter(1011095).then(item => console.log(item));
+
 
 ReactDOM.render(
   <React.StrictMode>
